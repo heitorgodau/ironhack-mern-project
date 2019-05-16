28159566a11e000f634f51d999c364853922c368
Heitor Godau <heitorgodau@gmail.com>
2019-05-15 21:58:33 -0300

    Create perfil and navbar component

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -3,16 +3,20 @@ import { Route, Switch} from 'react-router-dom';
 import Home from '../home/Home';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
+import Profile from '../profile/Profile';
+import Navbar from '../navbar/Navbar';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
+        <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={Signup} />
+          <Route exact path='/profile' component={Profile} />
         </Switch>
       </div>
     );
