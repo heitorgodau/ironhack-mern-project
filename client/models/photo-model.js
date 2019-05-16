c6d63c0ab7ba551e552af2a27413296df3a0acfe
Arnold Lafuente <thanos_alu001@hotmail.com>
2019-05-16 09:53:30 -0300

    Routes authentication

---
+++ client/models/photo-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const photoSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  consultation: [{ type: Schema.Types.ObjectId, ref: 'Consultation' }],
+});
+
+const photo = mongoose.model('photo', photoSchema);
+module.exports = photo;
