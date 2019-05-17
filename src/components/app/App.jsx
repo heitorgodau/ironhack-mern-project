22cac8b54300d85855c5ddf4d9d4e7b76a1f7764
Heitor Godau <heitorgodau@gmail.com>
2019-05-17 13:42:10 -0300

    Navbar and App organized. No more main.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,23 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Menu from '../navbar/Menu';
-import MenuItem from '../navbar/MenuItem';
-import MenuButton from '../navbar/MenuButton';
-import AuthService from './../auth/auth-service';
-import './App.css';
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import { Route, Switch} from 'react-router-dom';
+import Home from '../home/Home';
+import Login from '../login/Login';
+import Signup from '../signup/Signup';
+import Profile from '../profile/Profile';
+import Patient from '../patient/Patient';
+import AuthService from '../auth/auth-service';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      menuOpen:false,
-    }
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = { loggedInUser: null };
     this.service = new AuthService();
-    console.log("propssssssssss",props)
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({...this.state, loggedInUser: nextProps["userInSession"]})
+  fetchUser(){
+    if( this.state.loggedInUser === null ){
+      this.service.loggedin()
+      .then(response =>{
+        console.log(response)
+        this.setState({
+          loggedInUser:  response
+        }) 
+      })
+      .catch( err =>{
+        this.setState({
+          loggedInUser:  false
+        }) 
+      })
+    }
   }
 
   getTheUser= (userObj) => {
@@ -26,68 +38,33 @@ class App extends Component {
     })
   }
 
-  handleMenuClick() {
-    this.setState({menuOpen:!this.state.menuOpen});
-  }
-  
-  handleLinkClick() {
-    this.setState({menuOpen: false});
-  }
-
-  logoutUser = () =>{
-    this.service.logout()
-    .then(() => {
-      this.setState({ loggedInUser: null });
-      this.props.getUser(null);  
-      console.log("Success logout!!!")
-    })
-  }
-
-  render(){   
-    const styles= 
-      {
-        container:{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          zIndex: '99',
-          opacity: 0.9,
-          display:'flex',
-          alignItems:'center',
-          background: 'black',
-          width: '100%',
-          color: 'white',
-          fontFamily:'Lobster',
-        },
-        logo: {
-          margin: '0 auto',
-        },
-        body: {
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '100vw',
-          height: '100vh',
-          filter: this.state.menuOpen ? 'blur(2px)':null,
-          transition: 'filter 0.5s ease',
-        },
-      }
-
-    return (
-     
-      <div className="App">
-        <div style={styles.container}>
-          <MenuButton open={this.state.menuOpen} onClick={()=>this.handleMenuClick()} color='white'/>
-          <div style={styles.logo}>WireHeart</div>
+  render(){     
+    this.fetchUser() 
+    console.log(this.state)    
+    if(this.state.loggedInUser){ 
+      return (
+        <div className="app">
+          <Navbar userInSession={this.state.loggedInUser} getUser= {this.getTheUser}/>
+          <Switch>
+            <Route exact path='/' component={Profile} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/patient/:id' component={Patient} />
+          </Switch>
         </div>
-        <Menu open={this.state.menuOpen}>
-          <Link to='/'><MenuItem delay='0s' onClick={()=>{this.handleLinkClick();}}>Home</MenuItem></Link>
-          <Link to='/'><MenuItem delay='0.1s' onClick={()=>{this.handleLinkClick();}}>Minha Conta</MenuItem></Link>
-          <Link to='/'><MenuItem delay='0.2s' onClick={()=>{this.handleLinkClick();}}>Pacientes</MenuItem></Link>
-          <Link to='/'><MenuItem delay='0.3s' onClick={() => this.logoutUser()}>Logout</MenuItem></Link>
-        </Menu>
-      </div>
-    );
+      );
+    } else{
+      return (
+        <div className="app">            
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' render={() => <Login getUser={this.getTheUser}/>}/>
+            <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/> 
+          </Switch>
+        </div>
+      );
+    }  
   }
 }
 
